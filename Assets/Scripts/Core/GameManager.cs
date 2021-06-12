d7f0ef962d9ae77d3d425c68d0343a257935581d
Sergio Oporto <ser.op.te@gmail.com>
2021-06-12 10:48:51 -0400

    Add doors

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviourSingleton<GameManager>
+{
+    public UnityEvent LevelCompleted;
+
+    private bool _leftComleted = false;
+    private bool _rightComleted = false;
+
+    private void Start()
+    {
+        if (LevelCompleted == null)
+            LevelCompleted = new UnityEvent();
+    }
+
+    public void CompletedDoor(bool left)
+    {
+        if (left)
+        {
+            _leftComleted = true;
+        }
+        else
+        {
+            _rightComleted = true;
+        }
+
+        if (_leftComleted && _rightComleted)
+        {
+            LevelCompleted.Invoke();
+            Debug.Log("Finished");
+        }
+    }
+
+    public void ExitedDoor(bool left)
+    {
+        if (left)
+        {
+            _leftComleted = false;
+        }
+        else
+        {
+            _rightComleted = false;
+        }
+    }
+}
