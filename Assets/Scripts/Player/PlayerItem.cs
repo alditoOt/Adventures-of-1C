43a8c4d0a3a08ec9a1ecae0ea00a546efea143f7
Sergio Oporto <ser.op.te@gmail.com>
2021-06-12 16:11:11 -0400

    Add item UI

--- Assets/Scripts/Player/PlayerItem.cs
+++ Assets/Scripts/Player/PlayerItem.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerItem : MonoBehaviour
 {
     public ItemData item;
+    public ItemUI itemUI;
     private Item _item;
     private PlayerIdentifier _id;
-    private bool isUsing = false;
+    private bool isAvailable = false;
+    private bool wasUsed = false;
 
     private void Start()
     {
@@ -22,6 +25,8 @@ public class PlayerItem : MonoBehaviour
         {
             return null;
         }
+        isAvailable = true;
+        itemUI.SetImage(data.Icon);
         switch (data.Type)
         {
             case ItemType.MOVEMENT:
@@ -50,14 +55,14 @@ public class PlayerItem : MonoBehaviour
 
     private void UseItem()
     {
-        if (!isUsing)
+        if (isAvailable)
         {
             Debug.Log($"Using item from { (_id.IsLeft ? "left" : "right") } character");
             if (_item != null)
             {
                 var other = FindObjectsOfType<PlayerIdentifier>().First(p => p.IsLeft != _id.IsLeft).gameObject;
                 _item.Use(gameObject, other);
-                isUsing = true;
+                isAvailable = false;
                 StartCoroutine(RestoreCoroutine());
             }
         }
@@ -65,10 +70,11 @@ public class PlayerItem : MonoBehaviour
 
     private IEnumerator RestoreCoroutine()
     {
+        itemUI.HideImage(_item.data.Duration);
         yield return new WaitForSeconds(_item.data.Duration);
         var other = FindObjectsOfType<PlayerIdentifier>().First(p => p.IsLeft != _id.IsLeft).gameObject;
         Debug.Log($"Restoring item in { (_id.IsLeft ? "left" : "right") } character");
         _item.Restore(gameObject, other);
-        isUsing = false;
+        wasUsed = true;
     }
 }
---
+++ Assets/Scripts/UI/ItemUI.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUI : MonoBehaviour
+{
+    public Image frontImage;
+    public Image backImage;
+
+    public void SetImage(Sprite image)
+    {
+        frontImage.enabled = true;
+        backImage.enabled = true;
+        frontImage.sprite = image;
+        backImage.sprite = image;
+        backImage.color = new Color(backImage.color.r, backImage.color.g, backImage.color.b, 0.3f);
+        RestoreImage();
+    }
+
+    public void HideImage(float duration)
+    {
+        frontImage.DOFillAmount(0, duration);
+    }
+
+    public void RestoreImage()
+    {
+        frontImage.fillAmount = 1;
+    }
+}
